aa525b8a22f6c33fa7ccb75e53718bc81276740e
mohammad_rizwan <rizwanm76@yahoo.com>
2023-06-21 19:54:57 -0400

    fixed bug, needed to add data turbo false field to link_to tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,9 @@ class Post < ApplicationRecord
 
   validates :images, presence: true, blob: { content_type: :image } 
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :likers, through: :likes, source: :user
 
   
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
